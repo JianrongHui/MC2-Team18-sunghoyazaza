d982417033a2fab3a934ca816ecf17609c7d6a98
chad0909@naver.com <108044639+chad0909@users.noreply.github.com>
2023-05-06 22:27:39 +0900

    [#12] Feat: 앱 시작 시 권한설정 페이지 주석 생성

--- SungHozaza/SungHozaza/View/StartPermissionView.swift
+++ SungHozaza/SungHozaza/View/StartPermissionView.swift
@@ -4,13 +4,16 @@
 //
 //  Created by 077tech on 2023/05/06.
 //
+//MARK: 앱 시작 시 나오는 권한설정 화면
 
 import SwiftUI
 
+//MARK: MAIN
 struct StartPermissionView: View {
     var body: some View {
         ZStack{
             VStack{
+                //TODO: APP이름 정해지면 넣을 것
                 Text("\"App Name\"은 \n아래 기능을 사용해요")
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding()
@@ -19,6 +22,7 @@ struct StartPermissionView: View {
                 
 
                 Spacer()
+                // 권한 설정 하기 버튼
                 Button {
                     print("권한 설정하기 clicked")
                 } label: {
@@ -33,11 +37,13 @@ struct StartPermissionView: View {
 
         }
             VStack{
+                // 중간 Bold 텍스트 (UI 변경시 삭제될 것)
                 Text("Screen Time \n권한을 허용해주세요.")
                     .multilineTextAlignment(.center)
                     .bold()
                     .font(.system(size: 34))
                     .padding(.bottom, 1)
+                // 중간 Regular 텍스트 (UI 변경시 삭제될 것)
                 Text("앱을 사용하려면 \nScreen Time 권한이 필요합니다.")
                     .multilineTextAlignment(.center)
                     .font(.system(size: 17))
@@ -47,6 +53,7 @@ struct StartPermissionView: View {
     }
 }
 
+//MARK: PREVIEW
 struct StartPermissionView_Previews: PreviewProvider {
     static var previews: some View {
         StartPermissionView()
