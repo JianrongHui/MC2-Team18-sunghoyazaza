fcc7813b5094cb2958f6331aa2eee53e097fc9f3
yuncoffee <dong072815@gmail.com>
2023-05-08 14:52:28 +0900

    [#29] Feat: ScreenTime Extension 추가

---
+++ sunghoyazaza/sunghoyazaza/Utils/NotificationManager.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationManager.swift
+//  sunghoyazaza
+//
+//  Created by Yun Dongbeom on 2023/05/08.
+//
+
+import Foundation
+
+class NotificationManager {
+    static let shared = NotificationManager()
+    private init() {}
+}
---
+++ sunghoyazaza/sunghoyazaza/ViewModels/ScreenTImeVM.swift
@@ -0,0 +1,16 @@
+//
+//  ScreenTImeVM.swift
+//  sunghoyazaza
+//
+//  Created by Yun Dongbeom on 2023/05/08.
+//
+
+import Foundation
+
+// MARK: ScreenTime과 관련된 데이터를 관리하기 위한 Class
+class ScreenTimeVM: ObservableObject {
+    static let shared = ScreenTimeVM()
+    private init() {}
+    
+    
+}
