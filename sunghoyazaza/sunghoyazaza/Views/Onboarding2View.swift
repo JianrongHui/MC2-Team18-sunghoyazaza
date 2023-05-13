dfe4bf7e58be1386ff3c6c89ba1c98211f55bfb1
yuncoffee <dong072815@gmail.com>
2023-05-14 01:11:34 +0900

    [#74] Test: 아이콘 테스트

--- sunghoyazaza/sunghoyazaza/Views/Onboarding2View.swift
+++ sunghoyazaza/sunghoyazaza/Views/Onboarding2View.swift
@@ -7,14 +7,54 @@
 
 import SwiftUI
 import FamilyControls
+import ManagedSettings
+
+struct ColorfulIconLabelStyle: LabelStyle {
+    var color: Color
+    var size: CGFloat
+    
+    func makeBody(configuration: Configuration) -> some View {
+        Label {
+            configuration.title.hidden()
+        } icon: {
+            configuration.icon
+                .aspectRatio(10, contentMode: .fill)
+                .imageScale(.large)
+                .frame(width: 50, height: 50)
+        }
+    }
+}
 
 struct Onboarding2View: View {
     
-    @State var selection = ScreenTimeVM.shared.selectionToDiscourage
+    @State var selection = FamilyActivitySelection() {
+        didSet {
+            ScreenTimeVM.shared.selectionToDiscourage = selection
+        }
+    }
+    
     @State var isPresented = false
+    var tokens: [ApplicationToken] {
+        didSet {
+            print("GGG!")
+        }
+    }
+    
+    init() {
+        self.tokens = Array(ScreenTimeVM.shared.selectionToDiscourage.applicationTokens)
+        print(ScreenTimeVM.shared.selectionToDiscourage)
+    }
+    
+    let columns = [
+        GridItem(.fixed(56)),
+        GridItem(.fixed(56)),
+        GridItem(.fixed(56)),
+        GridItem(.fixed(56)),
+        GridItem(.fixed(56))
+    ]
     
     var body: some View {
-        VStack() {
+        VStack {
             VStack(alignment: .leading, spacing: 8) {
                 Text("사용을 제한할 앱을 선택해주세요").font(.largeTitle.bold())
                 Text("7시간 이상의 숙면은 내일의 집중을 도와줍니다.").foregroundColor(.gray)
@@ -29,27 +69,44 @@ struct Onboarding2View: View {
                 Spacer()
                 Button("편집") { isPresented = true }
                     .familyActivityPicker(isPresented: $isPresented,
-                                          selection: $selection)
+                  selection: $selection)
             }
             
             Spacer().frame(height: 8.0)
             
             // 앱 아이콘 나오는 부분
-            ForEach (0 ..< selection.applicationTokens.count, id: \.self) { index in
-                Label(Array(selection.applicationTokens)[index])
+            
+            LazyVGrid(columns: columns, alignment: .leading, spacing: 18){
+                if selection.applicationTokens.count > 0 {
+                    ForEach(Array(selection.applicationTokens), id: \.self) {
+                        token in
+                        HStack {
+                            Label(token)
+                                .labelStyle(.iconOnly)
+                                .scaleEffect(3)
+                        }
+                        .frame(width: 56, height: 56)
+                    }
+                }
+                if selection.categoryTokens.count > 0 {
+                    ForEach(Array(selection.categoryTokens), id: \.self) {
+                        token in
+                        HStack {
+                            Label(token)
+                                .labelStyle(.iconOnly)
+                                .scaleEffect(2.0)
+                        }
+                        .frame(width: 56, height: 56)
+                        .border(.blue)
+                    }
+                    .border(.red)
+                }
             }
-            
-            RoundedRectangle(cornerRadius: 16)
-                .fill(Color.gray)
-                .frame(height: 120)
-            
-            
-//            ForEach (selection.applicationTokens) { application in
-//                Label(application)
-//            }
-            
+            .padding(CGFloat.spacing16)
+            .frame(maxWidth: .infinity, minHeight: 80)
+            .background(Color.primary3)
+            .cornerRadius(16)
             Spacer()
-            
             NavigationLink(destination: {
                 MainView()
             }) {
@@ -67,5 +124,10 @@ struct Onboarding2View: View {
                 }
             }
             .background(Color.systemGray6, ignoresSafeAreaEdges: .all)
+            .onChange(of: selection) {
+                authStatus in
+                print("ggg")
+                
+            }
     }
 }
