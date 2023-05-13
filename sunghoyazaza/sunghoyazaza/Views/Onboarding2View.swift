da4f39b80f90428435df47460d946b397cfac7ae
Seokmin Jung <79754613+shine-jung@users.noreply.github.com>
2023-05-14 03:14:03 +0900

    [#74] Feat: Onboarding, Detail View UI 및 기능 완성

--- sunghoyazaza/sunghoyazaza/Views/Onboarding2View.swift
+++ sunghoyazaza/sunghoyazaza/Views/Onboarding2View.swift
@@ -54,71 +54,93 @@ struct Onboarding2View: View {
     ]
     
     var body: some View {
-        VStack {
-            VStack(alignment: .leading, spacing: 8) {
-                Text("사용을 제한할 앱을 선택해주세요").font(.largeTitle.bold())
-                Text("7시간 이상의 숙면은 내일의 집중을 도와줍니다.").foregroundColor(.gray)
-            }.frame(maxWidth: .infinity, alignment: .leading)
-            
-            // TODO::Pick interface
-            
-            // VERSION 1
-            HStack {
-                Text("제한 중인 앱 목록").font(.subheadline)
-                    .foregroundColor(.gray)
-                Spacer()
-                Button("편집") { isPresented = true }
-                    .familyActivityPicker(isPresented: $isPresented,
-                  selection: $selection)
-            }
-            
-            Spacer().frame(height: 8.0)
-            
-            // 앱 아이콘 나오는 부분
-            
-            LazyVGrid(columns: columns, alignment: .leading, spacing: 18){
-                if selection.applicationTokens.count > 0 {
-                    ForEach(Array(selection.applicationTokens), id: \.self) {
-                        token in
-                        HStack {
-                            Label(token)
-                                .labelStyle(.iconOnly)
-                                .scaleEffect(3)
-                        }
-                        .frame(width: 56, height: 56)
-                    }
+        VStack(spacing: 0) {
+            VStack(alignment: .leading, spacing: 0) {
+                Spacer().frame(height: 24.0)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("계획한 수면 시간을 설정해주세요").font(.largeTitle.bold())
+                    Text("7시간 이상의 숙면은 내일의 집중을 도와줍니다.").foregroundColor(.gray)
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer().frame(height: 24.0)
+                
+                // TODO::Pick interface
+                
+                // VERSION 1
+                HStack(spacing: 0) {
+                    Text("제한 중인 앱 목록").font(.subheadline)
+                        .foregroundColor(.gray)
+                    Spacer()
+                    Button("편집") { isPresented = true }
+                        .familyActivityPicker(isPresented: $isPresented,
+                                              selection: $selection).font(.subheadline).padding(.horizontal, 10.0).padding(.vertical, 4.0)
+                        .background(.white)
+                        .border(.white, width: 0)
+                        .cornerRadius(16)
                 }
-                if selection.categoryTokens.count > 0 {
-                    ForEach(Array(selection.categoryTokens), id: \.self) {
-                        token in
-                        HStack {
-                            Label(token)
-                                .labelStyle(.iconOnly)
-                                .scaleEffect(2.0)
+                
+                
+                Spacer().frame(height: 8.0)
+                
+                // 앱 아이콘 나오는 부분
+                
+                if (selection.applicationTokens.count > 0 || selection.categoryTokens.count > 0) {
+                    LazyVGrid(columns: columns, alignment: .leading, spacing: 18){
+                        if selection.applicationTokens.count > 0 {
+                            ForEach(Array(selection.applicationTokens), id: \.self) {
+                                token in
+                                HStack {
+                                    Label(token)
+                                        .labelStyle(.iconOnly)
+                                        .scaleEffect(3)
+                                }
+                                .frame(width: 56, height: 56)
+                            }
                         }
-                        .frame(width: 56, height: 56)
-                        .border(.blue)
+                        if selection.categoryTokens.count > 0 {
+                            ForEach(Array(selection.categoryTokens), id: \.self) {
+                                token in
+                                HStack {
+                                    Label(token)
+                                        .labelStyle(.iconOnly)
+                                        .scaleEffect(2.0)
+                                }
+                                .frame(width: 56, height: 56)
+                            }
+                        }
                     }
-                    .border(.red)
+                    .padding(CGFloat.spacing16)
+                    .frame(maxWidth: .infinity, minHeight: 80)
+                    .background(Color.primary3)
+                    .cornerRadius(16)
                 }
-            }
-            .padding(CGFloat.spacing16)
-            .frame(maxWidth: .infinity, minHeight: 80)
-            .background(Color.primary3)
-            .cornerRadius(16)
+                else {
+                    Text("선택된 앱이 없습니다.").foregroundColor(Color.systemGray)
+                        .padding(CGFloat.spacing16)
+                        .frame(maxWidth: .infinity, minHeight: 80)
+                        .background(Color.primary3)
+                        .cornerRadius(16)
+                }
+                
+                
+            }.padding(.horizontal, 24)
+            
             Spacer()
+            
             NavigationLink(destination: {
                 MainView()
             }) {
                 Text("시작하기").foregroundColor(.white)
-            }.padding()
-                .frame(width: 240)
+            }.padding().frame(maxWidth: .infinity)
+                .foregroundColor(.systemWhite)
                 .background(Color.accentColor)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                .padding([.horizontal, .bottom], CGFloat.spacing24)
         }.simultaneousGesture(TapGesture().onEnded{
             // MARK: 선택한 제한 앱 @AppStorage 변수에 저장
             ScreenTimeVM.shared.selectionToDiscourage = selection
-        }).padding().navigationBarTitleDisplayMode(.inline)
+        }).navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .principal) {
                     VStack {
